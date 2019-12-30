9e670dd0f1970e9729221f4072c3c20e8c37e69d
Eric Frus <eric.frus@gmail.com>
2019-12-30 09:34:41 -0600

    initil let parsing

--- src/parser.rs
+++ src/parser.rs
@@ -9,19 +9,91 @@ pub struct Parser<'a> {
 }
 
 impl<'a> Parser<'a> {
-    pub fn new(mut lexer: Lexer<'a>) -> Parser<'a> {
-        let current_token = None;
-        let peek_token = lexer.next_token();
+    pub fn new(lexer: Lexer<'a>) -> Parser<'a> {
+        let mut p = Parser {
+            lexer,
+            current_token: None,
+            peek_token: None,
+        };
 
-        Parser {
-            lexer: lexer,
-            current_token,
-            peek_token,
+        p.next_token();
+        p.next_token();
+
+        p
+    }
+
+    fn next_token(&mut self) {
+        self.current_token = self.peek_token.clone();
+        self.peek_token = self.lexer.next_token();
+    }
+
+    pub fn parse_program(&mut self) -> Program {
+        let mut statements = vec![];
+        let mut not_eof = true;
+
+        while not_eof {
+            match &self.current_token {
+                Some(_token) => {
+                    if let Some(statement) = self.parse_statement() {
+                        statements.push(statement);
+                    }
+                    self.next_token();
+                }
+                None => not_eof = false,
+            };
+        }
+        Program { statements }
+    }
+
+    fn parse_statement(&mut self) -> Option<Statement> {
+        if let Some(token) = &self.current_token {
+            let statement = match token {
+                Token::Let => self.parse_let_statement(),
+                _ => None,
+            };
+            return statement;
+        }
+        None
+    }
+
+    fn parse_let_statement(&mut self) -> Option<Statement> {
+        if let Some(Token::Ident(identifier)) = self.peek_token.clone() {
+            self.next_token();
+
+            if !self.expect_peek(Token::Assign) {
+                return None;
+            }
+
+            while !self.current_token_is(&Token::Semicolon) {
+                self.next_token();
+            }
+
+            let expression = Expression::None;
+            return Some(Statement::Let(identifier, expression));
+        }
+        None
+    }
+
+    fn current_token_is(&self, t: &Token) -> bool {
+        match &self.current_token {
+            Some(token) if token == t => true,
+            _ => false,
         }
     }
 
-    pub fn parse_program(&self) -> Option<Program> {
-        None
+    fn peek_token_is(&self, t: &Token) -> bool {
+        match &self.peek_token {
+            Some(token) if token == t => true,
+            _ => false,
+        }
+    }
+
+    fn expect_peek(&mut self, t: Token) -> bool {
+        if self.peek_token_is(&t) {
+            self.next_token();
+            return true;
+        }
+        false
     }
 }
 
@@ -41,21 +113,17 @@ mod tests {
         ";
 
         let lexer = Lexer::new(input);
-        let parser = Parser::new(lexer);
+        let mut parser = Parser::new(lexer);
 
-        if let Some(program) = parser.parse_program() {
-            assert_eq!(3, program.statements.len());
+        let program = parser.parse_program();
+        assert_eq!(3, program.statements.len());
 
-            let tests = ["x", "y", "foobar"];
+        let tests = ["x", "y", "foobar"];
 
-            let mut statements = program.statements.into_iter();
-            for test in tests.iter() {
-                let s = statements.next().unwrap();
-                test_let_statement(s, test);
-            }
-        } else {
-            println!("Parse Program returned None!");
-            assert!(false);
+        let mut statements = program.statements.into_iter();
+        for test in tests.iter() {
+            let s = statements.next().unwrap();
+            test_let_statement(s, test);
         }
     }
 
